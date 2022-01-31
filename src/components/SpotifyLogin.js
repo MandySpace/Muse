3a2a0154b5cdbaf2f21e18586d0d998940bbd0b1
MandySpace <amandeepsingh1994@gmail.com>
2022-01-31 18:41:40 +0530

    Revamped welcome page

--- src/components/SpotifyLogin.js
+++ src/components/SpotifyLogin.js
@@ -1,3 +1,5 @@
+import girl from "../girl.svg";
+
 const CLIENT_ID = "0df5782ff13e4b0389221a08ff5be16d";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = "https://muse-music-player.netlify.app/";
@@ -17,10 +19,18 @@ function SpotifyLogin() {
 
   return (
     <div className="login-page">
-      <h1 className="login-title">Welcome to Muse</h1>
+      <div className="login-text">
+        <h1 className="login-title">Welcome to Muse</h1>
+        <p className="login-desc">
+          Click on get started to view your favourite playlists and tracks!
+        </p>
+      </div>
       <button className="login-btn" onClick={handleLogin}>
         Get Started
       </button>
+      <div className="svg-cont">
+        <img src={girl} alt="" className="svg" />
+      </div>
     </div>
   );
 }
